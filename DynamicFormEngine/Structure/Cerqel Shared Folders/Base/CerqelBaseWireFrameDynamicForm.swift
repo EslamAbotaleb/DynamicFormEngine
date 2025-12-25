ead073ec1981f984f567e793308592f354fe581f
Eslam Baiomy <ebaiomy@youxel.com>
2025-12-25 19:33:18 +0200

    still working on revamp dynamic form

--- DynamicFormEngine/Structure/Cerqel Shared Folders/Base/CerqelBaseWireFrameDynamicForm.swift
+++ DynamicFormEngine/Structure/Cerqel Shared Folders/Base/CerqelBaseWireFrameDynamicForm.swift
@@ -282,7 +282,7 @@ class CerqelBaseWireFrameDynamicForm<T: CerqelBaseViewModel>: BottomSheetVCCerqe
             label.textColor = .whiteCerqel
             label.font = UIFont.caption3Regular()
             label.textAlignment = .center
-            label.backgroundColor =UIColor.error_Cerqel
+            label.backgroundColor = UIColor.error_Cerqel
             label.layer.cornerRadius = label.frame.width / 2
             label.layer.masksToBounds = true
             if notificationCounter >= 100 {
